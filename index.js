1b4417e25374f559737c29718b7110745895a989
Himanshupachouri34 <himasnhupachouri34@gmail.com>
2023-10-04 06:34:06 +0530

    okay

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const passwordBox = document.getElementById("Password");
+const length = 8;
+
+const upperCase = " ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+const lowerCase = " abcdefghijklmnopqrstuvwxyz ";
+const number = "1234567890";
+const symbols = "!@#$%&*<>?";
+
+const allChars = upperCase + lowerCase + number + symbols;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)]
+
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+
+    password += number[Math.floor(Math.random() * number.length)]
+
+    password += symbols[Math.floor(Math.random() * symbols.length)]
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+    }
+
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
